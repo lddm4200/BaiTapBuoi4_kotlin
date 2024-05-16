8beb689d71cfeb26682410eecf3b8edd6cf5cb4d
Minh Long <longddm2k4@gmail.com>
2024-05-16 12:17:37 +0700

    mm

---
+++ app/src/main/java/com/example/baitapbuoi3/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.example.baitapbuoi3
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.baitapbuoi3.ui.theme.BaiTapBuoi3Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+
+
+        }
+    }
+}
+
+@Composable
+fun MessageCard(msg: String) {
+    Text(
+        text = msg,
+        modifier = Modifier
+            .padding(0.dp, 16.dp)
+            .fillMaxWidth(),
+        color = Color.DarkGray,
+        fontSize = 20.sp,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center,
+    )
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BaiTapBuoi3Theme {
+        MessageCard("Đào Duy Minh Long - PH42469")
+        GreetingCard()
+    }
+}
+
+@Composable
+fun GreetingCard() {
+    var text by remember { val mutableStateOf = mutableStateOf("")
+        mutableStateOf
+    }
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        MessageCard(msg = text)
+        Button(onClick = { text = "Hi!" }) {
+            Text("Say Hi!")
+        }
+    }
+}
